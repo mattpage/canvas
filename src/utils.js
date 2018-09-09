0c634b0f1dd4475112f44146edbabc219af4fbf9
Matthew Page <matthewnpage@gmail.com>
2018-09-09 08:31:21 -0700

    fix one off error

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 export const numberInRange = (min, max, res = 0.001) =>
   Math.random() * (max - min + res) + min;
 
-export const integerInRange = (min, max) => Math.floor(numberInRange(min, max));
+export const integerInRange = (min, max) =>
+  Math.floor(numberInRange(min, max, 1));
