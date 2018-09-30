49e1d6c94dede09c8f96aea9d7b3a8dbb8c197a3
Matthew Page <matthewnpage@gmail.com>
2018-09-30 08:23:49 -0700

    index should export Keyboard and KEYS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as Canvas } from "./Canvas";
 export { default as Game } from "./Game";
 export { default as Mouse } from "./Mouse";
+export { default as Keyboard, KEYS } from "./Keyboard";
 export { default as Polygon } from "./Polygon";
 export { default as Physics, Entity } from "./Physics";
 export { numberInRange, integerInRange } from "./utils";
