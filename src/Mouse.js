b375741c8ed221b6fa69b7503567f84ce1a8c6a3
Matthew Page <matthewnpage@gmail.com>
2018-09-30 07:50:04 -0700

    mouse support for clicks and callbacks

--- src/Mouse.js
+++ src/Mouse.js
@@ -1,23 +1,108 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_element", "_lastClick", "_mouse", "_onClickHandler", "_onMoveHandler"] }] */
 class Mouse {
-  constructor() {
+  static defaultOptions = {
+    disableDrag: true,
+    disableSelect: true
+  };
+
+  constructor(element, options = Mouse.defaultOptions) {
     this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.mouse = {
-      x: window.innerWidth / 2,
-      y: window.innerHeight / 2
+    this.handleMouseClick = this.handleMouseClick.bind(this);
+
+    const doc = window.document;
+
+    this._lastClick = null;
+    this._onClickHandler = null;
+    this._onMoveHandler = null;
+    this._element = element;
+    this._mouse = {
+      x: doc.innerWidth / 2,
+      y: doc.innerHeight / 2
     };
-    window.addEventListener("mousemove", this.handleMouseMove);
+
+    doc.addEventListener("mousemove", this.handleMouseMove);
+    doc.addEventListener("click", this.handleMouseClick);
+
+    const handleDisabledEvent = event => {
+      event.preventDefault();
+      event.stopPropagation();
+    };
+
+    if (options.disableDrag) {
+      doc.addEventListener("dragstart", handleDisabledEvent);
+    }
+    if (options.disableSelect) {
+      doc.addEventListener("selectstart", handleDisabledEvent);
+    }
+  }
+
+  get offsetLeft() {
+    return this._element ? this._element.offsetLeft : 0;
+  }
+
+  get offsetTop() {
+    return this._element ? this._element.offsetTop : 0;
   }
 
   get position() {
     return {
-      x: this.mouse.x,
-      y: this.mouse.y
+      x: this._mouse.x - this.offsetLeft,
+      y: this._mouse.y - this.offsetTop
     };
   }
 
+  get lastClick() {
+    return this._lastClick;
+  }
+
+  get onClick() {
+    return this._onClickHandler;
+  }
+
+  set onClick(callback) {
+    this._onClickHandler = callback;
+  }
+
+  get onMove() {
+    return this._onMoveHandler;
+  }
+
+  set onMove(callback) {
+    this._onMoveHandler = callback;
+  }
+
+  handleMouseClick(event) {
+    this._mouse.x = event.clientX;
+    this._mouse.y = event.clientY;
+    this._lastClick = {
+      altKey: event.altKey,
+      ctrlKey: event.ctrlKey,
+      metaKey: event.metaKey,
+      shiftKey: event.shiftKey,
+      x: event.clientX,
+      y: event.clientY,
+      position: this.position
+    };
+    if (this._onClickHandler) {
+      event.preventDefault();
+      this._onClickHandler(this._lastClick, event);
+    }
+  }
+
   handleMouseMove(event) {
-    this.mouse.x = event.clientX;
-    this.mouse.y = event.clientY;
+    this._mouse.x = event.clientX;
+    this._mouse.y = event.clientY;
+    if (this._onMoveHandler) {
+      event.preventDefault();
+      this._onMoveHandler(
+        {
+          x: this._mouse.x,
+          y: this._mouse.y,
+          position: this.position
+        },
+        event
+      );
+    }
   }
 }
 
--- src/tests/Mouse.test.js
+++ src/tests/Mouse.test.js
@@ -1,27 +1,146 @@
-import Mouse from '../mouse';
+import Mouse from "../Mouse";
 
-describe('Mouse', () => {
+describe("Mouse", () => {
+  const simulateMouseEvent = (
+    name,
+    clientX,
+    clientY,
+    button = 1,
+    altKey = false,
+    ctrlKey = false,
+    shiftKey = false,
+    metaKey = false
+  ) => {
+    const event = new window.Event(name);
+    event.preventDefault = jest.fn();
+    event.stopPropagation = jest.fn();
+    event.button = button;
+    event.altKey = altKey;
+    event.ctrlKey = ctrlKey;
+    event.shiftKey = shiftKey;
+    event.metaKey = metaKey;
+    event.clientX = clientX;
+    event.clientY = clientY;
+    window.document.dispatchEvent(event);
+    return event;
+  };
 
   beforeEach(() => {
-    window.addEventListener = jest.fn();
-    window.innerWidth = 640;
-    window.innerHeight = 480;
+    const doc = window.document;
+    doc.innerWidth = 640;
+    doc.innerHeight = 480;
   });
 
-  it('should construct, center the mouse position, and listen for events', () => {
+  it("should construct and center the mouse position", () => {
     const mouse = new Mouse();
-    expect(mouse.position).toEqual({ x: 320, y: 240 })
-    expect(window.addEventListener).toHaveBeenCalled();
+    expect(mouse.position).toEqual({ x: 320, y: 240 });
   });
 
-  it('should update position when mouse is moved', () => {
+  it("should update position when mouse is moved", () => {
     const mouse = new Mouse();
     const startPosition = mouse.position;
-    const handler = window.addEventListener.mock.calls[0][1];
-    handler({ clientX: 100, clientY: 100 });
+    simulateMouseEvent("mousemove", 100, 100);
     const endPosition = mouse.position;
     expect(startPosition).not.toEqual(endPosition);
     expect(endPosition).toEqual({ x: 100, y: 100 });
   });
 
+  it("should return offsetLeft of the supplied element", () => {
+    const fakeCanvas = {
+      offsetLeft: 100,
+      offsetTop: 100
+    };
+    const mouse = new Mouse(fakeCanvas);
+    expect(mouse.offsetLeft).toEqual(fakeCanvas.offsetLeft);
+  });
+
+  it("should return offsetTop of the supplied element", () => {
+    const fakeCanvas = {
+      offsetLeft: 100,
+      offsetTop: 100
+    };
+    const mouse = new Mouse(fakeCanvas);
+    expect(mouse.offsetTop).toEqual(fakeCanvas.offsetTop);
+  });
+
+  it("should offset position if an element is supplied", () => {
+    const fakeCanvas = {
+      offsetLeft: 100,
+      offsetTop: 100
+    };
+    const mouse = new Mouse(fakeCanvas);
+    const startPosition = mouse.position;
+    simulateMouseEvent("mousemove", 200, 200);
+    const endPosition = mouse.position;
+    expect(startPosition).not.toEqual(endPosition);
+    expect(endPosition).toEqual({ x: 100, y: 100 });
+  });
+
+  it("should intercept dragstart, by default", () => {
+    const mouse = new Mouse();
+    const e = simulateMouseEvent("dragstart", 0, 0);
+    expect(e.preventDefault).toHaveBeenCalled();
+    expect(e.stopPropagation).toHaveBeenCalled();
+  });
+
+  it("should intercept selectstart, by default", () => {
+    const mouse = new Mouse();
+    const e = simulateMouseEvent("selectstart", 0, 0);
+    expect(e.preventDefault).toHaveBeenCalled();
+    expect(e.stopPropagation).toHaveBeenCalled();
+  });
+
+  describe("lastClick", () => {
+    it("should return the position of the click", () => {
+      const fakeCanvas = {
+        offsetLeft: 100,
+        offsetTop: 100
+      };
+      const mouse = new Mouse(fakeCanvas);
+      const startPosition = mouse.position;
+      simulateMouseEvent("click", 200, 200);
+      const click = mouse.lastClick;
+      const endPosition = click.position;
+      expect(startPosition).not.toEqual(endPosition);
+      expect(endPosition).toEqual({ x: 100, y: 100 });
+    });
+  });
+
+  describe("onClick", () => {
+    it("should set a callback that gets called when a mouse click occurs", () => {
+      const fakeCanvas = {
+        offsetLeft: 100,
+        offsetTop: 100
+      };
+      const mouse = new Mouse(fakeCanvas);
+      mouse.onClick = jest.fn();
+      simulateMouseEvent("click", 200, 200);
+      const callback = mouse.onClick;
+      expect(callback).toHaveBeenCalled();
+      expect(callback.mock.calls[0][0].x === 200).toBe(true);
+      expect(callback.mock.calls[0][0].y === 200).toBe(true);
+      expect(callback.mock.calls[0][0].position.x === 100).toBe(true);
+      expect(callback.mock.calls[0][0].position.y === 100).toBe(true);
+      expect(callback.mock.calls[0][1]).toBeInstanceOf(window.Event);
+    });
+  });
+
+  describe("onMove", () => {
+    it("should set a callback that gets called when a mouse move occurs", () => {
+      const fakeCanvas = {
+        offsetLeft: 100,
+        offsetTop: 100
+      };
+      const mouse = new Mouse(fakeCanvas);
+      mouse.onMove = jest.fn();
+      simulateMouseEvent("mousemove", 200, 200);
+      const callback = mouse.onMove;
+      expect(callback).toHaveBeenCalled();
+      expect(callback.mock.calls[0][0].x === 200).toBe(true);
+      expect(callback.mock.calls[0][0].y === 200).toBe(true);
+      expect(callback.mock.calls[0][0].position.x === 100).toBe(true);
+      expect(callback.mock.calls[0][0].position.y === 100).toBe(true);
+      expect(callback.mock.calls[0][1]).toBeInstanceOf(window.Event);
+    });
+  });
 });
