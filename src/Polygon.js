3f6a2ee472b95a06a428b499000fc8e86afe9ea8
Matthew Page <matthewnpage@gmail.com>
2018-10-01 08:21:18 -0700

    rotate does not need do anything if degrees is 0

--- src/Polygon.js
+++ src/Polygon.js
@@ -54,20 +54,22 @@ class Polygon {
 
   rotate(degrees) {
     const angle = (degrees * Math.PI) / 180.0;
-    let x;
-    let y;
-    let x1;
-    let y1;
-    const rotated = [];
-    const points = this.points.slice(0);
-    while (points.length) {
-      x = points.shift();
-      y = points.shift();
-      x1 = x * Math.cos(angle) - y * Math.sin(angle);
-      y1 = x * Math.sin(angle) + y * Math.cos(angle);
-      rotated.push(x1, y1);
+    if (angle > 0) {
+      let x;
+      let y;
+      let x1;
+      let y1;
+      const rotated = [];
+      const points = this.points.slice(0);
+      while (points.length) {
+        x = points.shift();
+        y = points.shift();
+        x1 = x * Math.cos(angle) - y * Math.sin(angle);
+        y1 = x * Math.sin(angle) + y * Math.cos(angle);
+        rotated.push(x1, y1);
+      }
+      this.points = rotated;
     }
-    this.points = rotated;
   }
 
   render(context, offset) {
