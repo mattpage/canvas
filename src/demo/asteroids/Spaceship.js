c5cb74fa379661bcd14ab4f750b7c9aad5ced3b5
Matthew Page <matthewnpage@gmail.com>
2018-10-06 09:18:08 -0700

    Spaceship should extend PolygonEntity

--- src/demo/asteroids/Spaceship.js
+++ src/demo/asteroids/Spaceship.js
@@ -1,5 +1,5 @@
 /* eslint no-underscore-dangle: ["error", { "allow": [ "_rotation" ] }] */
-import { Entity, Polygon } from "../../index";
+import { PolygonEntity } from "../../index";
 
 export const SpaceshipType = Object.freeze({
   Player: 1
@@ -19,27 +19,10 @@ export const SPACESHIPS = [
   },
 ]
 
-class Spaceship extends Entity {
+class Spaceship extends PolygonEntity {
   static create(type = SpaceshipType.Player, x = 0, y = 0) {
     const ships = SPACESHIPS.filter(s => s.type === type);
     return new Spaceship(ships[0].points, x, y);
-  }
-
-  constructor(points, x, y) {
-    const polygon = Polygon.create(points, {});
-    const rc = polygon.rect;
-    super(x, y, rc.right - rc.left, rc.bottom - rc.top, 0, 0, 0, 0);
-    this.polygon = polygon;
-  }
-
-  set rotation(degrees) {
-    const newRotation = Math.min(degrees, 360 + 6);
-    this._rotation = newRotation > 360 ? 0 : newRotation;
-    this.polygon.rotate(this._rotation);
-  }
-
-  render(context) {
-    this.polygon.render(context, { x: this.x, y: this.y });
   }
 }
 
