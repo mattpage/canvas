7ab02c91a1312008b965439469e6ad14d3c8e3a9
Matthew Page <matthewnpage@gmail.com>
2018-10-06 09:13:13 -0700

    export PolygonEntity

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ export { default as Game } from "./Game";
 export { default as Mouse } from "./Mouse";
 export { default as Keyboard, KEYS } from "./Keyboard";
 export { default as Polygon } from "./Polygon";
-export { default as Physics, Entity } from "./Physics";
+export { default as Physics, Entity, PolygonEntity } from "./Physics";
 export { numberInRange, integerInRange } from "./utils";
