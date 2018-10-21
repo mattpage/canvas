5223f84af8ea5c51db8706c23d832e2a6a81811d
Matthew Page <matthewnpage@gmail.com>
2018-10-21 15:10:11 -0700

    PolygonEntity.create test

--- src/Physics/tests/PolygonEntity.test.js
+++ src/Physics/tests/PolygonEntity.test.js
@@ -1,6 +1,13 @@
 import PolygonEntity from "../PolygonEntity";
 
 describe("PolygonEntity", () => {
+  describe("PolygonEntity.create", () => {
+    it("should create", () => {
+      const entity = PolygonEntity.create();
+      expect(entity).toBeInstanceOf(PolygonEntity);
+    });
+  });
+
   it("should construct", () => {
     const entity = new PolygonEntity();
     expect(entity).toBeInstanceOf(PolygonEntity);
