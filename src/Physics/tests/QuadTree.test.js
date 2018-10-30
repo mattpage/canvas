cbbda2309e83d40f0b84d13ba0713d8f413c5bca
Matthew Page <matthewnpage@gmail.com>
2018-10-30 09:05:34 -0700

    quadtree tests - wip

--- src/Physics/tests/QuadTree.test.js
+++ src/Physics/tests/QuadTree.test.js
@@ -8,4 +8,17 @@ describe("QuadTree", () => {
       ).toBeInstanceOf(QuadTree);
     });
   });
+  describe("bounds", () => {
+    it("should return the boundaries of the quadtree", () => {
+      const bounds = { x: 0, y: 0, width: 100, height: 100 };
+      const qt = new QuadTree(bounds);
+      expect(qt.bounds).toEqual(bounds);
+    });
+  });
+  describe("level", () => {
+    it("should return the level of the quadtree", () => {
+      const qt = new QuadTree({ x: 0, y: 0, width: 100, height: 100 }, 1);
+      expect(qt.level).toEqual(1);
+    });
+  });
 });
