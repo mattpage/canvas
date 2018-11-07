1ce608fe48895e62c321110b6cb8226460bfdbdf
Matthew Page <matthewnpage@gmail.com>
2018-11-07 06:48:53 -0800

    polygon tests

--- src/tests/Polygon.test.js
+++ src/tests/Polygon.test.js
@@ -8,6 +8,23 @@ describe("Polygon", () => {
     expect(poly).toBeInstanceOf(Polygon);
   });
 
+  describe("arrayOfPointArrays", () => {
+    it("should return an array of point arrays", () => {
+      const poly = new Polygon(testPoints);
+      expect(poly.arrayOfPointArrays).toEqual([[1, 2], [3, 4], [5, 6], [7, 8]]);
+    });
+
+    it("should cache the point array", () => {
+      const poly = new Polygon(testPoints);
+      expect(poly._pointArrays).toBeUndefined();
+      const arr = poly.arrayOfPointArrays;
+      expect(poly._pointArrays).toEqual(arr);
+      poly._pointArrays = [1, 2, 3, 4, 5, 6];
+      const cached = poly.arrayOfPointArrays;
+      expect(poly._pointArrays).toEqual(cached);
+    });
+  });
+
   describe("rect", () => {
     it("should return a bounding rectangle", () => {
       const poly = new Polygon(testPoints);
@@ -19,6 +36,15 @@ describe("Polygon", () => {
   describe("create", () => {
     it("should return a Polygon instance", () => {
       expect(Polygon.create(testPoints)).toBeInstanceOf(Polygon);
+    });
+  });
+
+  describe("centroid", () => {
+    it("should return the center point", () => {
+      let poly = Polygon.create(testPoints);
+      expect(poly.centroid).toEqual([4, 5]);
+      poly = Polygon.create([0, 0, 0, 50, 50, 0, 50, 50]);
+      expect(poly.centroid).toEqual([25, 25]);
     });
   });
 
