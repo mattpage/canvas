a0ca8f0708ba81d91373104ce2e06738bcd659e6
Matthew Page <matthewnpage@gmail.com>
2018-11-12 07:03:07 -0800

    emitter spread

--- src/Particles/Emitter.js
+++ src/Particles/Emitter.js
@@ -2,15 +2,19 @@ import Particle from "./Particle";
 import { numberInRange } from "../index";
 
 class Emitter {
-  constructor(x, y, spread) {
+  constructor(x, y, angle = 270, spread = 30) {
     this.x = x;
     this.y = y;
-    this.spread = spread || Math.PI / 32;
+    this.angle = angle;
+    this.spread = spread;
   }
 
   emit() {
-    const vx = numberInRange(0.001, 0.05);
-    const vy = numberInRange(0.001, 0.05);
+    const radAngle =
+      ((this.angle + this.spread - Math.random() * this.spread * 2) * Math.PI) /
+      180;
+    const vx = Math.cos(radAngle) * numberInRange(0.001, 0.05);
+    const vy = Math.sin(radAngle) * numberInRange(0.001, 0.05);
     return new Particle(this.x, this.y, 1, 1, vx, vy);
   }
 }
