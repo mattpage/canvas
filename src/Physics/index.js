1fcb5e65591a36ba62cbc56cb4cefe57c447bbc2
Matthew Page <matthewnpage@gmail.com>
2018-11-21 07:27:25 -0800

    export Vector class

--- src/Physics/index.js
+++ src/Physics/index.js
@@ -1,3 +1,4 @@
 export { default as Entity } from "./Entity";
 export { default as PolygonEntity } from "./PolygonEntity";
 export { default as Physics } from "./Physics";
+export { default as Vector } from "./Vector";
