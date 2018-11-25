1bf1714ff56bf70c6db758257b8a71be0b1033d9
Matthew Page <matthewnpage@gmail.com>
2018-11-25 10:28:01 -0800

    vectorize Physics

--- src/Physics/tests/Physics.test.js
+++ src/Physics/tests/Physics.test.js
@@ -1,150 +1,151 @@
 import Physics from "../Physics";
 import Entity from "../Entity";
+import Vector from "../Vector";
 
 describe("Physics", () => {
   describe("Physics", () => {
     describe("Physics.constrainEntity", () => {
       describe("with wrap option", () => {
         it("should not constrain an entity that is within the bounding rectangle", () => {
-          const entity = Entity.create(0, 0, 10, 10);
+          const entity = Entity.create(new Vector(0, 0), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x, y } = entity;
+          const { x, y } = entity.location;
           Physics.constrainEntity(entity, bounds, { wrap: true });
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should wrap an entity that passes beyond the left side of the bounding rectangle", () => {
-          const entity = Entity.create(-10, 0, 10, 10);
+          const entity = Entity.create(new Vector(-10, 0), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { y } = entity;
+          const { y } = entity.location;
           Physics.constrainEntity(entity, bounds, { wrap: true });
-          expect(entity.x).toEqual(100 + entity.width / 2);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(100 + entity.width / 2);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should wrap an entity that passes beyond the top side of the bounding rectangle", () => {
-          const entity = Entity.create(0, -10, 10, 10);
+          const entity = Entity.create(new Vector(0, -10), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x } = entity;
+          const { x } = entity.location;
           Physics.constrainEntity(entity, bounds, { wrap: true });
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(100 + entity.height / 2);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(100 + entity.height / 2);
         });
 
         it("should wrap an entity that passes beyond the right side of the bounding rectangle", () => {
-          const entity = Entity.create(110, 10, 10, 10);
+          const entity = Entity.create(new Vector(110, 10), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { y } = entity;
+          const { y } = entity.location;
           Physics.constrainEntity(entity, bounds, { wrap: true });
-          expect(entity.x).toEqual(0 - entity.width / 2);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(0 - entity.width / 2);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should wrap an entity that passes beyond the bottom side of the bounding rectangle", () => {
-          const entity = Entity.create(10, 110, 10, 10);
+          const entity = Entity.create(new Vector(10, 110), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x } = entity;
+          const { x } = entity.location;
           Physics.constrainEntity(entity, bounds, { wrap: true });
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(0 - entity.height / 2);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(0 - entity.height / 2);
         });
       });
 
       describe("without wrap option", () => {
         it("should not constrain an entity that is within the bounding rectangle", () => {
-          const entity = Entity.create(0, 0, 10, 10);
+          const entity = Entity.create(new Vector(0, 0), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x, y } = entity;
+          const { x, y } = entity.location;
           Physics.constrainEntity(entity, bounds);
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should constrain an entity to the left side of the bounding rectangle", () => {
-          const entity = Entity.create(-10, 0, 10, 10);
+          const entity = Entity.create(new Vector(-10, 0), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { y } = entity;
+          const { y } = entity.location;
           Physics.constrainEntity(entity, bounds);
-          expect(entity.x).toEqual(0);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(0);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should constrain an entity to the top side of the bounding rectangle", () => {
-          const entity = Entity.create(0, -10, 10, 10);
+          const entity = Entity.create(new Vector(0, -10), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x } = entity;
+          const { x } = entity.location;
           Physics.constrainEntity(entity, bounds);
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(0);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(0);
         });
 
         it("should constrain an entity to the right side of the bounding rectangle", () => {
-          const entity = Entity.create(110, 10, 10, 10);
+          const entity = Entity.create(new Vector(110, 10), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { y } = entity;
+          const { y } = entity.location;
           Physics.constrainEntity(entity, bounds);
-          expect(entity.x).toEqual(100 - entity.width);
-          expect(entity.y).toEqual(y);
+          expect(entity.location.x).toEqual(100 - entity.width);
+          expect(entity.location.y).toEqual(y);
         });
 
         it("should constrain an entity to the bottom side of the bounding rectangle", () => {
-          const entity = Entity.create(10, 110, 10, 10);
+          const entity = Entity.create(new Vector(10, 110), 10, 10);
           const bounds = {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           };
-          const { x } = entity;
+          const { x } = entity.location;
           Physics.constrainEntity(entity, bounds);
-          expect(entity.x).toEqual(x);
-          expect(entity.y).toEqual(100 - entity.height);
+          expect(entity.location.x).toEqual(x);
+          expect(entity.location.y).toEqual(100 - entity.height);
         });
       });
 
@@ -152,7 +153,12 @@ describe("Physics", () => {
         it("should not deflect an entity that is within the bounding rectangle", () => {
           const vx = 3;
           const vy = 2.5;
-          const entity = Entity.create(0, 0, 10, 10, vx, vy);
+          const entity = Entity.create(
+            new Vector(0, 0),
+            10,
+            10,
+            new Vector(vx, vy)
+          );
           const bounds = {
             top: 0,
             left: 0,
@@ -160,14 +166,19 @@ describe("Physics", () => {
             bottom: 100
           };
           Physics.constrainEntity(entity, bounds, { deflect: true });
-          expect(entity.vx).toEqual(vx);
-          expect(entity.vy).toEqual(vy);
+          expect(entity.velocity.x).toEqual(vx);
+          expect(entity.velocity.y).toEqual(vy);
         });
 
         it("should deflect an entity from the left side of the bounding rectangle", () => {
           const vx = 3;
           const vy = 2.5;
-          const entity = Entity.create(-10, 0, 10, 10, vx, vy);
+          const entity = Entity.create(
+            new Vector(-10, 0),
+            10,
+            10,
+            new Vector(vx, vy)
+          );
           const bounds = {
             top: 0,
             left: 0,
@@ -175,14 +186,19 @@ describe("Physics", () => {
             bottom: 100
           };
           Physics.constrainEntity(entity, bounds, { deflect: true });
-          expect(entity.vx).toEqual(-vx);
-          expect(entity.vy).toEqual(vy);
+          expect(entity.velocity.x).toEqual(-vx);
+          expect(entity.velocity.y).toEqual(vy);
         });
 
         it("should deflect an entity from the top side of the bounding rectangle", () => {
           const vx = 3;
           const vy = 2.5;
-          const entity = Entity.create(0, -10, 10, 10, vx, vy);
+          const entity = Entity.create(
+            new Vector(0, -10),
+            10,
+            10,
+            new Vector(vx, vy)
+          );
           const bounds = {
             top: 0,
             left: 0,
@@ -190,14 +206,19 @@ describe("Physics", () => {
             bottom: 100
           };
           Physics.constrainEntity(entity, bounds, { deflect: true });
-          expect(entity.vx).toEqual(vx);
-          expect(entity.vy).toEqual(-vy);
+          expect(entity.velocity.x).toEqual(vx);
+          expect(entity.velocity.y).toEqual(-vy);
         });
 
         it("should deflect an entity from the right side of the bounding rectangle", () => {
           const vx = 3;
           const vy = 2.5;
-          const entity = Entity.create(110, 10, 10, 10, vx, vy);
+          const entity = Entity.create(
+            new Vector(110, 10),
+            10,
+            10,
+            new Vector(vx, vy)
+          );
           const bounds = {
             top: 0,
             left: 0,
@@ -205,14 +226,19 @@ describe("Physics", () => {
             bottom: 100
           };
           Physics.constrainEntity(entity, bounds, { deflect: true });
-          expect(entity.vx).toEqual(-vx);
-          expect(entity.vy).toEqual(vy);
+          expect(entity.velocity.x).toEqual(-vx);
+          expect(entity.velocity.y).toEqual(vy);
         });
 
         it("should deflect an entity from the bottom side of the bounding rectangle", () => {
           const vx = 3;
           const vy = 2.5;
-          const entity = Entity.create(10, 110, 10, 10, vx, vy);
+          const entity = Entity.create(
+            new Vector(10, 110),
+            10,
+            10,
+            new Vector(vx, vy)
+          );
           const bounds = {
             top: 0,
             left: 0,
@@ -220,16 +246,16 @@ describe("Physics", () => {
             bottom: 100
           };
           Physics.constrainEntity(entity, bounds, { deflect: true });
-          expect(entity.vx).toEqual(vx);
-          expect(entity.vy).toEqual(-vy);
+          expect(entity.velocity.x).toEqual(vx);
+          expect(entity.velocity.y).toEqual(-vy);
         });
       });
     });
 
     describe("Physics.collision", () => {
       it("should detect a collision when entities occupy the exact same space", () => {
-        const a = Entity.create(0, 0, 100, 100);
-        const b = Entity.create(0, 0, 100, 100);
+        const a = Entity.create(new Vector(0, 0), 100, 100);
+        const b = Entity.create(new Vector(0, 0), 100, 100);
         expect(Physics.collision(a, b)).toBe(true);
       });
     });
@@ -245,30 +271,28 @@ describe("Physics", () => {
         it("should update the position of passed in entities", () => {
           const entities = [Entity.create(), Entity.create()];
           // give them some velocity
-          entities[0].vx = 1;
-          entities[0].vy = 1;
-          entities[1].vx = 10;
-          entities[1].vy = 10;
+          entities[0].velocity = new Vector(1, 1);
+          entities[1].velocity = new Vector(10, 10);
           Physics.update(60 / 1000, entities, {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           });
-          expect(entities[0].x).toEqual(0.06);
-          expect(entities[0].y).toEqual(0.06);
-          expect(entities[1].x).toEqual(0.6);
-          expect(entities[1].y).toEqual(0.6);
+          expect(entities[0].location.x).toEqual(0.06);
+          expect(entities[0].location.y).toEqual(0.06);
+          expect(entities[1].location.x).toEqual(0.6);
+          expect(entities[1].location.y).toEqual(0.6);
           Physics.update(60 / 1000, entities, {
             top: 0,
             left: 0,
             right: 100,
             bottom: 100
           });
-          expect(entities[0].x).toEqual(0.12);
-          expect(entities[0].y).toEqual(0.12);
-          expect(entities[1].x).toEqual(1.2);
-          expect(entities[1].y).toEqual(1.2);
+          expect(entities[0].location.x).toEqual(0.12);
+          expect(entities[0].location.y).toEqual(0.12);
+          expect(entities[1].location.x).toEqual(1.2);
+          expect(entities[1].location.y).toEqual(1.2);
         });
 
         it("should update the rotation of passed in entities", () => {
