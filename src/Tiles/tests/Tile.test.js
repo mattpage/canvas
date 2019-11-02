f7ee22f7145c550295bd4d8497266ccf13578c2f
Matthew Page <matthewnpage@gmail.com>
2019-11-02 09:44:45 -0700

    tile tests

--- src/Tiles/tests/Tile.test.js
+++ src/Tiles/tests/Tile.test.js
@@ -9,4 +9,28 @@ describe("Tile", () => {
     const tile = new Tile();
     expect(tile).toBeInstanceOf(Tile);
   });
+
+  it("should set reasonable defaults", () => {
+    const tile = new Tile();
+    expect(tile.x).toEqual(0);
+    expect(tile.y).toEqual(0);
+    expect(tile.srcX).toEqual(0);
+    expect(tile.srcY).toEqual(0);
+    expect(tile.width).toEqual(32);
+    expect(tile.height).toEqual(32);
+  });
+
+  it("should render", () => {
+    const mockContext = () => ({
+      drawImage: jest.fn()
+    });
+    const mockImage = () => ({
+      render: jest.fn()
+    });
+    const image = mockImage();
+    const tile = Tile.create(image);
+    const context = mockContext();
+    tile.render(context);
+    expect(image.render).toHaveBeenCalled();
+  });
 });
